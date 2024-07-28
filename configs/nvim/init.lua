d4633dd225e0832808fdd5e1d3c80d5cc9b0dc8f
xiaoming <mail@zyming.me>
2024-07-28 20:40:24 +0200

    Set tabstop in init.lua instead of relying on vimrc

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,6 +1,5 @@
 -- Source vanilla vimrc
 local vim = vim
-local o = vim.opt
 
 vim.cmd("source ~/.vimrc")
 
@@ -8,6 +7,12 @@ vim.cmd("source ~/.vimrc")
 require("init_nvim_tree")
 require("setup_ccc")
 require("colorizer_init")
+
+-- set tabs
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
 
 -- set g options
 vim.g.airline_powerline_fonts=1
@@ -19,3 +24,4 @@ vim.keymap.set("n", "<C-g>",
     end)
 
 vim.cmd("colorscheme flow")
+ 
