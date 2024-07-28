39faef019cb1811e1b06416f5c91c95f54ccd847
xiaoming <mail@zyming.me>
2024-07-28 13:34:51 +0200

    Move the init of nvimTree to it's own lua module

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,26 +1,4 @@
 -- Source vanilla vimrc
 local vim = vim
 vim.cmd("source ~/.vimrc")
-
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
-
--- Enable Nvim-tree
-require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
+require("init_nvim_tree")
