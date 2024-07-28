d737b901e00b9cea2b74b81dc97729c84c818c51
xiaoming <mail@zyming.me>
2024-07-28 14:17:56 +0200

    add keybinding to show nvim-tree

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,4 +1,12 @@
 -- Source vanilla vimrc
 local vim = vim
 vim.cmd("source ~/.vimrc")
+
+-- import plugin configurations
 require("init_nvim_tree")
+
+-- Keybinds
+vim.keymap.set("n", "<C-g>",
+    function()
+        vim.cmd("NvimTreeToggle")
+    end)
