1f1ea69b5f4c2df2d2e1295175f62924c18b00f2
Xiaoming94 <mail@zyming.me>
2024-08-01 08:27:56 +0200

    Add some archlinux specific aliases to standardalias.zsh

--- configs/zshrc.d/autoload/standardalias.zsh
+++ configs/zshrc.d/autoload/standardalias.zsh
@@ -3,4 +3,13 @@
 alias ls='ls -F --color=auto'
 alias ll='ls -F -al --color=auto'
 
-alias less='less -R'
+alias grep='grep --color=auto --exclude-dir=.cvs --exclude-dir=.git --exclude-dir=.hg --exclude-dir=.svn'
+
+# Arch Linux aliases
+alias pacupg='sudo pacman -Syu'
+alias pacin='sudo pacman -S'
+alias pacins='sudo pacman -U'
+alias pacfind='pacman -Ss'
+alias pacflocal='pacman -Q'
+alias pacinf='pacman -Qi'
+alias pacrepi='pacman -Si'
