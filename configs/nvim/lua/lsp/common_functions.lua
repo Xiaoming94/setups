06ad0c286bef32d9fd0814678bc662462e5a346e
Ming <mail@zyming.me>
2024-08-08 14:48:10 +0200

    Start working on LSP functions

---
+++ configs/nvim/lua/lsp/common_functions.lua
@@ -0,0 +1,18 @@
+local lsp = vim.lsp
+
+local function preview_location_callback(_, method, result)
+    if result == nil or vim.tbl_isempty(result) then
+        lsp.log.info(method, 'No location found')
+        return nil
+    end
+    if vim.tbl_islist(result) then
+        lsp.util.preview_location(result[1])
+    else
+        lsp.util.preview_location(result)
+    end
+end
+
+function peek_definition()
+    local params = lsp.util.make_position_params()
+    return lsp.buf_request(0, 'textDocument/definition', params, preview_location_callback)
+end
