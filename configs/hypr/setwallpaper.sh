c81a8b708dbcc4595b1fc1cd8a59cbed5d239091
Xiaoming <mail@zyming.me>
2024-08-12 21:41:16 +0200

    Modify wallpaper script and add keybind for lock-session

--- configs/hypr/setwallpaper.sh
+++ configs/hypr/setwallpaper.sh
@@ -9,6 +9,5 @@ wallpapers+=($(ls -d /usr/share/hyprland/wall*))
 wall=${wallpapers[ $RANDOM % ${#wallpapers[@]} ]}
 
 hyprctl hyprpaper preload $wall
-for monitor in $(hyprctl monitors | grep 'Monitor' | awk '{ print $2 }'); do
-    hyprctl hyprpaper wallpaper "$monitor,$wall"
-done
+hyprctl hyprpaper wallpaper ,$wall
+
