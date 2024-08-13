6f73e7d6ea0216dc06b54786704aa60e56f498b1
Ming <mail@zyming.me>
2024-08-13 11:16:37 +0200

    Change keybindings in nvim-tree

--- configs/nvim/lua/init_nvim_tree.lua
+++ configs/nvim/lua/init_nvim_tree.lua
@@ -24,7 +24,7 @@ local function my_on_attach(bufnr)
     vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
     vim.keymap.set('n', '?',     api.tree.toggle_help,           opts('help'))
     vim.keymap.set('n', '<C-s>', api.node.open.vertical,         opts('Open: Vertical Split'))
-    vim.keymap.set('n', '<C-i>', api.node.open.horizontal,       opts('Open: Horizontal Split'))
+    vim.keymap.set('n', '<C-x>', api.node.open.horizontal,       opts('Open: Horizontal Split'))
 
 end
 
