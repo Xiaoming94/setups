97d0f51ca050319cce664484eb0c50c8b6399c8e
Ming <mail@zyming.me>
2024-11-12 16:46:05 +0100

    Add minimap settings and wraps to neovim

--- configs/nvim/lua/minimap_init.lua
+++ configs/nvim/lua/minimap_init.lua
@@ -1,7 +1,4 @@
-vim.g.minimap_width = 10
-
--- Setting Keybindings for showing the minimap for 
-vim.keymap.set("n", "<C-m>", -- Ctrl + M
-    function()
-        vim.cmd("MinimapToggle")
-    end)
+vim.g.neominimap = {
+    auto_enable = true,
+    layout = "float",
+}
