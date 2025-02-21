1383bc16803f9895d3e506f36c3d6698ceabfa47
Ming <mail@zyming.me>
2025-02-21 11:06:15 +0100

    add dates to blame_lines

--- configs/nvim/lua/blame_lines.lua
+++ configs/nvim/lua/blame_lines.lua
@@ -3,10 +3,9 @@
 vim.g.blameLineUseVirtualText = 1
 vim.g.blameLineVirtualTextHighlight = 'Comment'
 vim.g.blameLineVirtualTextFormat = '%s'
-vim.g.blameLineGitFormat = '%cn | %h | %s'
+vim.g.blameLineGitFormat = '%cn | %h | %ar | %s'
 vim.g.blameLineMessageWhenNotYetCommited = 'uncommitted'
 
 -- keybinds
 
-vim.keymap.set('n','Gb', ":ToggleBlameLine<CR>", {})
-
+vim.keymap.set('n', 'Gb', ":ToggleBlameLine<CR>", {})
