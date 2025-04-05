b5bc41506245235a79c1a319bcf2343868dcf00c
Xiaoming <mail@zyming.me>
2025-04-05 15:47:23 +0200

    Correct setwallpaper.sh script to use correct path for hyprland wallpapers

--- configs/hypr/setwallpaper.sh
+++ configs/hypr/setwallpaper.sh
@@ -4,7 +4,7 @@ hyprctl hyprpaper unload all
 # Sets a random wallpaper with swaybg
 
 wallpapers=($(ls -d $HOME/.local/share/wallpapers/*.png))
-wallpapers+=($(ls -d /usr/share/hyprland/wall*))
+wallpapers+=($(ls -d /usr/share/hypr/wall*))
 
 wall=${wallpapers[ $RANDOM % ${#wallpapers[@]} ]}
 
