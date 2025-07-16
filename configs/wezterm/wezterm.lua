562f33a20c3f1642721221d31ccb3c5d54256cca
Xiaoming <mail@zyming.me>
2025-07-16 22:12:37 +0545

    Use blinking block cursour in wezterm

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -16,11 +16,16 @@ config.font = wezterm.font 'FiraCode Nerd Font'
 config.font_size = 10
 config.color_scheme = 'Aci (Gogh)'
 
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.90
 
 -- Hide Tabs
 config.hide_tab_bar_if_only_one_tab = true
 
+-- blinking cursor
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+config.default_cursor_style = "BlinkingBlock"
+config.cursor_blink_rate = 500
 
 -- Finally, return the configuration to wezterm:
 return config
