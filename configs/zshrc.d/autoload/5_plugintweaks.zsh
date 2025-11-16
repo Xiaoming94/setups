b46ee788f8d2ba87f23f62a777b60cbf0ee0f82f
Xiaoming <mail@zyming.me>
2025-11-16 20:51:27 +0100

    [ZSH] change zsh autosuggestions strategy

---
+++ configs/zshrc.d/autoload/5_plugintweaks.zsh
@@ -0,0 +1,7 @@
+################################
+# CONFIG FILE FOR TWEAKING ANTIDOTE PLUGINS
+################################
+
+# tweaking autosuggestion
+export ZSH_AUTOSUGGEST_STRATEGY=(completion history)
+
