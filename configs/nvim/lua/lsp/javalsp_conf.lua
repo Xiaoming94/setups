dc6af2529be49c0ef8408032f7080b288b4bbd78
Xiaoming <mail@zyming.me>
2025-12-30 11:55:41 +0100

    [nvim] change the priority order of root markers for jdtls

--- configs/nvim/lua/lsp/javalsp_conf.lua
+++ configs/nvim/lua/lsp/javalsp_conf.lua
@@ -1,1 +1,14 @@
+vim.lsp.config("jdtls", {
+    cmd = { "jdtls" },
+    root_markers = {
+        "gradlew",
+        "mvnw",
+        "pom.xml",
+        "build.gradle",
+        "build.gradle.kts",
+        ".git",
+    },
+})
+
+-- enable jdtls
 vim.lsp.enable('jdtls')
